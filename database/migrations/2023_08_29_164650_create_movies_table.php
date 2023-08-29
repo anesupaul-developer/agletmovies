f47b29e093ae102bc8be29d5bea9e1ff53725604
Anesu Ngirande <anesupaulngirande@gmail.com>
2023-08-29 18:59:19 +0200

    add movies migration and model

---
+++ database/migrations/2023_08_29_164650_create_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->char('uuid', 36)->unique();
+            $table->string('title');
+            $table->string('backdrop_path');
+            $table->bigInteger('movie_id')->unique();
+            $table->string('original_language')->default('en');
+            $table->string('original_title');
+            $table->text('overview');
+            $table->decimal('popularity', 18, 2)->nullable();
+            $table->string('poster_path');
+            $table->date('release_date');
+            $table->decimal('vote_average', 18, 2)->nullable();
+            $table->integer('vote_count')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
